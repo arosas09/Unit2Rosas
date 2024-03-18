368bef4776be82535dc5887badd21334242392e8
arosas09 <arosas23001@gmail.com>
2024-03-18 11:42:32 -0700

    finished Game
    I finished the game i just have to finish the challlenges.

--- Unit2RosasP3/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2RosasP3/Assets/Scripts/DestroyOutOfBounds.cs
@@ -13,9 +13,12 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
-
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
+        }
              
     }
 }
--- Unit2RosasP3/Assets/Scripts/PlayerController.cs
+++ Unit2RosasP3/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 10;
     public GameObject projectilePrefab;
+    public float zMin;
+    public float zMax;
+
 
     // Update is called once per frame
     void Update()
     {
 
-        //Keep player in bounds.
+        //Keep player in bounds horizontal.
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -22,10 +26,23 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        //keep player in bounds vertical.
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        }
+
 
 
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
